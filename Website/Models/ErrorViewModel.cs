1f8258802b9dcaaba757fbf341552644b43a064c
brookhou73 <brookhou@gmail.com>
2025-12-31 10:51:33 -0600

    New Setup

---
+++ Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TopekaMustangClubInc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
