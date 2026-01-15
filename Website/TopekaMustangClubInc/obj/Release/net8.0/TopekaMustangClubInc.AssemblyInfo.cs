bfafddc3c21426f7fd2e144c9379c6f3470857b3
brookhou73 <brookhou@gmail.com>
2026-01-15 15:39:22 -0600

    Added a Theme

--- Website/TopekaMustangClubInc/obj/Release/net8.0/TopekaMustangClubInc.AssemblyInfo.cs
+++ Website/TopekaMustangClubInc/obj/Release/net8.0/TopekaMustangClubInc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac72128a507aa632940922400b4aed7e67fd5151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b612020e37b3ec5e69c0f261ca8a778635ae6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
