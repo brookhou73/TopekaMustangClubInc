dfc72dcaebfa0138ea6ac2e8a17d9e0952e63690
brookhou73 <brookhou@gmail.com>
2026-01-15 20:25:45 -0600

    Updated Wounded Warrior Project

--- Website/TopekaMustangClubInc/obj/Release/net8.0/TopekaMustangClubInc.AssemblyInfo.cs
+++ Website/TopekaMustangClubInc/obj/Release/net8.0/TopekaMustangClubInc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d2a73cf0008f26ef406225a0d997a509a1d9df4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db2daa12d74d6d41faa6e73414a0fc488883350")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
