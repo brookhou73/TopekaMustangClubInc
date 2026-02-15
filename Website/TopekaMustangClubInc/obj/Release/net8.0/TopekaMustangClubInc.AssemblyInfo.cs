171910055f8f53581add252382ed1266adffe7bb
brookhou73 <brookhou@gmail.com>
2026-02-14 19:38:27 -0600

    Update new sponsor Shannon Watts

--- Website/TopekaMustangClubInc/obj/Release/net8.0/TopekaMustangClubInc.AssemblyInfo.cs
+++ Website/TopekaMustangClubInc/obj/Release/net8.0/TopekaMustangClubInc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9347191f9ea491b52705a85c81363d714df47b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd09f457b4034b4512e4fa56c3e319e849ca22d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopekaMustangClubInc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
